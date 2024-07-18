6a3a80f46729dd43df6c8d86354396895d91b372
Tatang Romadhona <romadhonatatang@gmail.com>
2024-07-18 23:23:05 +0700

    handle media image

--- src/routers/v1/player.router.js
+++ src/routers/v1/player.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const auth = require('../../controllers/v1/player/auth.controller');
-const { validate } = require('../../middlewares/auth.middleware');
+const { validate, isAdmin, isSuperadmin } = require('../../middlewares/auth.middleware');
 const { image } = require('../../libs/multer');
 const media = require('../../controllers/v1/media.controller');
 
@@ -9,7 +9,7 @@ router.post('/auth/login', auth.login);
 router.get('/auth/whoami', validate, auth.whoami);
 
 // media
-router.post('/media/images', image.single('image'), media.imageKitUpload);
-router.get('/media/images', media.imageKitList);
+router.post('/media/images', validate, isAdmin, image.single('image'), media.imageKitUpload);
+router.get('/media/images', validate, isAdmin, media.imageKitList);
 
 module.exports = router;
