37cd30a850bd689d31a3d7cef9a9c0b3fba6beae
Tatang Romadhona <romadhonatatang@gmail.com>
2024-07-23 19:55:33 +0700

    unuse googleapis

--- src/controllers/v1/media.controller.js
+++ src/controllers/v1/media.controller.js
@@ -1,35 +1,35 @@
-const googleapis = require('../../libs/googleapis');
+// const googleapis = require('../../libs/googleapis');
 const imagekit = require('../../libs/imagekit');
 const path = require('path');
 
 module.exports = {
-    uploadImage: async (req, res, next) => {
-        try {
-            let response = await googleapis.upload(req.file);
-            return res.json({
-                status: true,
-                message: 'Image uploaded',
-                err: null,
-                data: response
-            });
-        } catch (error) {
-            next(error);
-        }
-    },
+    // uploadImage: async (req, res, next) => {
+    //     try {
+    //         let response = await googleapis.upload(req.file);
+    //         return res.json({
+    //             status: true,
+    //             message: 'Image uploaded',
+    //             err: null,
+    //             data: response
+    //         });
+    //     } catch (error) {
+    //         next(error);
+    //     }
+    // },
 
-    listImages: async (req, res) => {
-        try {
-            let response = await googleapis.list();
-            return res.json({
-                status: true,
-                message: 'List of images',
-                err: null,
-                data: response
-            });
-        } catch (error) {
-            next(error);
-        }
-    },
+    // listImages: async (req, res) => {
+    //     try {
+    //         let response = await googleapis.list();
+    //         return res.json({
+    //             status: true,
+    //             message: 'List of images',
+    //             err: null,
+    //             data: response
+    //         });
+    //     } catch (error) {
+    //         next(error);
+    //     }
+    // },
 
     imageKitUpload: async (req, res, next) => {
         try {
--- src/libs/googleapis.js
+++ src/libs/googleapis.js
@@ -1,81 +1,81 @@
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL, GOOGLE_ACCESS_TOKEN, DEFAULT_FOLDER_ID } = process.env;
+// const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL, GOOGLE_ACCESS_TOKEN, DEFAULT_FOLDER_ID } = process.env;
 
-const { google } = require('googleapis');
-const { Readable } = require('stream');
+// const { google } = require('googleapis');
+// const { Readable } = require('stream');
 
-const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL);
-oauth2Client.setCredentials({ access_token: GOOGLE_ACCESS_TOKEN });
+// const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL);
+// oauth2Client.setCredentials({ access_token: GOOGLE_ACCESS_TOKEN });
 
-const drive = google.drive({
-    version: 'v3',
-    auth: oauth2Client
-});
+// const drive = google.drive({
+//     version: 'v3',
+//     auth: oauth2Client
+// });
 
-async function grantPermission(fileId) {
-    try {
-        await drive.permissions.create({
-            fileId: fileId,
-            requestBody: {
-                role: 'reader',
-                type: 'anyone',
-            }
-        });
-        console.log('fileId', fileId);
-        return;
-    } catch (error) {
-        throw new Error(`Failed to grant permission: ${error.message}`);
-    }
-}
+// async function grantPermission(fileId) {
+//     try {
+//         await drive.permissions.create({
+//             fileId: fileId,
+//             requestBody: {
+//                 role: 'reader',
+//                 type: 'anyone',
+//             }
+//         });
+//         console.log('fileId', fileId);
+//         return;
+//     } catch (error) {
+//         throw new Error(`Failed to grant permission: ${error.message}`);
+//     }
+// }
 
-async function upload({ originalname, mimetype, buffer }) {
-    try {
-        const file = await drive.files.create({
-            resource: {
-                name: originalname,
-                parents: [DEFAULT_FOLDER_ID]
-            },
-            media: {
-                mimeType: mimetype,
-                body: Readable.from(buffer)
-            },
-            fields: 'id'
-        });
-        grantPermission(file.data.id);
-        return {
-            file_id: file.data.id,
-            file_name: originalname,
-            file_url: `https://drive.google.com/file/d/${file.data.id}/view`,
-            thumbnail_url: `https://drive.google.com/thumbnail?id=${file.data.id}&sz=w1000`
-        };
-    } catch (error) {
-        throw new Error(`Failed to upload file: ${error.message}`);
-    }
-}
+// async function upload({ originalname, mimetype, buffer }) {
+//     try {
+//         const file = await drive.files.create({
+//             resource: {
+//                 name: originalname,
+//                 parents: [DEFAULT_FOLDER_ID]
+//             },
+//             media: {
+//                 mimeType: mimetype,
+//                 body: Readable.from(buffer)
+//             },
+//             fields: 'id'
+//         });
+//         grantPermission(file.data.id);
+//         return {
+//             file_id: file.data.id,
+//             file_name: originalname,
+//             file_url: `https://drive.google.com/file/d/${file.data.id}/view`,
+//             thumbnail_url: `https://drive.google.com/thumbnail?id=${file.data.id}&sz=w1000`
+//         };
+//     } catch (error) {
+//         throw new Error(`Failed to upload file: ${error.message}`);
+//     }
+// }
 
-async function list() {
-    try {
-        const res = await drive.files.list({
-            pageSize: 10,
-            q: `'${DEFAULT_FOLDER_ID}' in parents and trashed = false`,
-            fields: 'nextPageToken, files(id, name)',
-        });
-        const files = res.data.files;
-        if (files.length) {
-            const filesWithUrls = await Promise.all(files.map(async (file) => {
-                return {
-                    file_id: file.id,
-                    file_name: file.name,
-                    file_url: `https://drive.google.com/file/d/${file.id}/view`,
-                    thumbnail_url: `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`
-                };
-            }));
-            return filesWithUrls;
-        } else {
-            return [];
-        }
-    } catch (error) {
-        throw new Error(`Failed to list files: ${error.message}`);
-    }
-}
+// async function list() {
+//     try {
+//         const res = await drive.files.list({
+//             pageSize: 10,
+//             q: `'${DEFAULT_FOLDER_ID}' in parents and trashed = false`,
+//             fields: 'nextPageToken, files(id, name)',
+//         });
+//         const files = res.data.files;
+//         if (files.length) {
+//             const filesWithUrls = await Promise.all(files.map(async (file) => {
+//                 return {
+//                     file_id: file.id,
+//                     file_name: file.name,
+//                     file_url: `https://drive.google.com/file/d/${file.id}/view`,
+//                     thumbnail_url: `https://drive.google.com/thumbnail?id=${file.id}&sz=w1000`
+//                 };
+//             }));
+//             return filesWithUrls;
+//         } else {
+//             return [];
+//         }
+//     } catch (error) {
+//         throw new Error(`Failed to list files: ${error.message}`);
+//     }
+// }
 
-module.exports = { upload, list, };
+// module.exports = { upload, list, };
