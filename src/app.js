346a1905d4ae07c02256878010d80f7d30ea92e5
Tatang Romadhona <romadhonatatang@gmail.com>
2024-09-13 01:19:09 +0700

    feat: add website link

--- src/app.js
+++ src/app.js
@@ -92,3 +92,60 @@ cron.schedule('*/10 * * * *', () => {
 cron.schedule('0 11 * * *', () => {
     resetFullEnergyQuota();
 });
+
+
+// telegram bot
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({ log: ['query'] });
+const bot = require('./libs/telegraf.js');
+
+bot.command('connect', async (ctx) => {
+    try {
+        const telegramId = String(ctx.from.id);
+        const player = await prisma.player.findFirst({
+            where: { telegram_id: telegramId }
+        });
+
+        if (!player) {
+            return ctx.reply('🚫 Sorry, you are not registered as a player. Please register to use this feature.');
+        }
+
+        if (player.phone_number && /^\+\d{10,15}$/.test(player.phone_number)) {
+            return ctx.reply(`🔗 Your phone number is already connected: ${player.phone_number}. No need to connect again.`);
+        }
+
+        ctx.reply('📱 Please send your phone number in the format: +1234567890. Note: This can be done only once.');
+    } catch (error) {
+        console.error('Error handling /connect command:', error);
+        ctx.reply('❗ Sorry, something went wrong. Please try again later.');
+    }
+});
+
+bot.on('message', async (ctx) => {
+    try {
+        const message = ctx.message.text;
+        const telegramId = String(ctx.from.id);
+        const player = await prisma.player.findFirst({
+            where: { telegram_id: telegramId }
+        });
+
+        // Handle phone number input
+        if (/^\+\d{10,15}$/.test(message)) {
+            if (player.phone_number && /^\+\d{10,15}$/.test(player.phone_number)) {
+                return ctx.reply(`🔗 Your phone number is already connected: ${player.phone_number}. No need to connect again.`);
+            }
+
+            await prisma.player.update({
+                where: { id: player.id },
+                data: { phone_number: message }
+            });
+            ctx.reply(`✅ Your phone number has been connected: ${message}`);
+        }
+    } catch (error) {
+        console.error('Error handling /connect command:', error);
+        ctx.reply('❗ Sorry, something went wrong. Please try again later.');
+    }
+});
+
+bot.launch();
+
